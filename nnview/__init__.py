96ee2b39568ac79249aded59407152999d8ac97c
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2019-05-11 21:10:25 +0530

    Package initialization

---
+++ nnview/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Package initialization.
+Created on Sat May 11 21:00:00 2019
+Author: Prasun Roy | CVPRU-ISICAL (http://www.isical.ac.in/~cvpr)
+GitHub: https://github.com/prasunroy/nnview
+
+"""
+
+
+__version__ = '0.1.0'
